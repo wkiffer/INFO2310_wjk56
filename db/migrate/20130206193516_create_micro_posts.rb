c8e27ec855169b011bec58053139db15b416ba20
Will Kiffer <wkiffer@gmail.com>
2013-02-13 18:24:57 +0000

    Deleted duplicate micro posts migration

--- db/migrate/20130206193516_create_micro_posts.rb
+++
@@ -1,9 +0,0 @@
-class CreateMicroPosts < ActiveRecord::Migration
-  def change
-    create_table :micro_posts do |t|
-      t.integer :user_id
-	  t.string :content
-      t.timestamps
-    end
-  end
-end
